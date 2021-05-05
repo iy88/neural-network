70fa5b4f8504687825d72509ccff5f03304d2223
iy88 <1845767798@qq.com>
2021-05-05 22:10:09 +0800

    update

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,34 +1,27 @@
 import { writeFileSync } from "fs";
-import { readFileSync } from "fs";
 import Network from "../core/v2/network";
-// import diffReLU from "../tools/diffReLu";
-// import LeakyReLU from "../tools/LeakyReLU";
-// import diffReLU from "../tools/diffReLu";
-// import diffSigmoid from "../tools/diffSigmoid";
-// import ReLU from "../tools/ReLU";
-// import sigmoid from "../tools/sigmoid";
+import sum from "../tools/sum";
 let n = new Network();
 n.addLayer(2, 10);
 n.addLayer(10, 1);
-let trainingData: { input: numberArray; output: numberArray; }[] = [
-  { input: [1,2], output: [(1+2)/2] },
-  { input: [3,4], output: [(3+4)/2] },
-  { input: [5,6], output: [(5+6)/2] },
-]
-let testingData:{input:numberArray,output:numberArray}[] = [
-  { input: [7,8], output: [(7+8)/2] },
-  { input: [9,10], output: [(9+10)/2] },
-  { input: [11,12], output: [(11+12)/2] },
-]
+let trainingData: { input: numberArray; output: numberArray; }[] = [];
+for (let i = 0; i < 100; i++) {
+  let x = Math.floor(Math.random() * 10);
+  let y = Math.floor(Math.random() * 10);
+  trainingData.push({ input: [x, y], output: [x + y] })
+}
 console.log('-----raw-----');
 console.time()
-console.log(n.fit(trainingData, testingData, 1e-3, Math.max(trainingData.length,testingData.length), 10000).length);
+n.train(trainingData, 1e-3, 10000);
 writeFileSync('./model.json', JSON.stringify(n.snapshot()));
 console.timeEnd();
-console.log(n.feedforward([1,2])[0],(1+2)/2);
-console.log(n.feedforward([2,3])[0],(2+3)/2);
-console.log('-----copy-----');
-let file = readFileSync('./model.json').toString()
-n.load(JSON.parse(file));
-console.log(n.feedforward([1,2])[0],(1+2)/2);
-console.log(n.feedforward([2,3])[0],(2+3)/2);
+let acts: numberArray = [];
+for (let i = 0; i < 10000; i++) {
+  let x = Math.floor(Math.random() * 10);
+  let y = Math.floor(Math.random() * 10);
+  let tr = x + y;
+  let act = (1 - ((n.feedforward([x, y])[0] - tr)) / tr);
+  isFinite(act) ? acts.push(act) : i--;
+}
+console.log("accuracy",sum(acts) / acts.length * 100);
+console.log(n.feedforward([1, 1])[0]);
