a934cde68c4ffb2c9309705d393ae84183883187
iy88 <1845767798@qq.com>
2021-05-09 20:46:09 +0800

    :art:

--- src/core/v2/neuron.ts
+++ src/core/v2/neuron.ts
@@ -1,39 +1,39 @@
 import vector from "../../tools/vector";
 
 class Neuron {
-  private weigths: numberArray = [];
+  private weights: numberArray = [];
   private bias: number = 0;
 
   constructor(shape: number, weights?: numberArray) {
     if (weights && weights.length !== 0) {
-      this.weigths = weights!.slice(0, -1);
+      this.weights = weights!.slice(0, -1);
       this.bias = weights!.slice(-1)[0];
     } else {
       for (let i = 0; i < shape; i++) {
-        this.weigths.push(Math.random());
+        this.weights.push(Math.random());
       }
     }
   }
 
   feedforward(inputs: numberArray) {
-    if (inputs.length === this.weigths.length) {
-      return vector.dot(this.weigths, inputs) + this.bias
+    if (inputs.length === this.weights.length) {
+      return vector.dot(this.weights, inputs) + this.bias
     } else {
       throw TypeError("inputs shape was not matched");
     }
   }
 
   backward(inputs: numberArray, derivative: number) {
-    let lastWeights = this.weigths.slice(0);
-    for (let i = 0; i < this.weigths.length; i++) {
-      this.weigths[i] -= derivative * inputs[i];
+    let lastWeights = this.weights.slice(0);
+    for (let i = 0; i < this.weights.length; i++) {
+      this.weights[i] -= derivative * inputs[i];
     }
     this.bias -= derivative;
     return lastWeights;
   }
 
   snapshot() {
-    return { weigths: this.weigths, bias: this.bias };
+    return { weigths: this.weights, bias: this.bias };
   }
 }
 
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,16 +1,16 @@
-import { writeFileSync,readFileSync} from "fs";
+// import { writeFileSync,readFileSync} from "fs";
 import Network from "../core/v2/network";
-import sum from "../tools/sum";
+// import sum from "../tools/sum";
 let n = new Network('crossEntropy');
-n.addLayer(2,2,'relu');
-n.addLayer(2,2,'relu');
-n.addLayer(2,1,'sigmoid');
+n.addLayer(1, 2, 'relu');
+// n.addLayer(2, 2, 'relu');
+n.addLayer(2, 1, 'relu');
 let trainingData: { input: numberArray; output: numberArray; }[] = [
-  {input:[1,0],output:[1]},
-  {input:[1,1],output:[0]},
-  {input:[0,1],output:[1]},
-  {input:[0,0],output:[0]}
-  // {input:[1],output:[1]},
+  // {input:[1,0],output:[1]},
+  // {input:[1,1],output:[0]},
+  // {input:[0,1],output:[1]},
+  // {input:[0,0],output:[0]}
+  { input: [1], output: [1] },
   // {input:[2],output:[2]},
 ];
 // for (let i = 0; i < 10; i++) {
@@ -21,20 +21,20 @@ let trainingData: { input: numberArray; output: numberArray; }[] = [
 // }
 // console.log(trainingData);
 console.time();
-writeFileSync('./loss',n.fit(trainingData,trainingData,1e-2,1000000,1000000).join('\n'));
-// n.train(trainingData,1e-4,1000000);
+// writeFileSync('./loss',n.fit(trainingData,trainingData,1e-2,1000000,1000000).join('\n'));
+n.train(trainingData, 1e-2, 100);
 // writeFileSync('./loss',loss.map(l=>l.join(' ')).join('\n'));
 console.timeEnd();
-writeFileSync('./model.json', JSON.stringify(n.snapshot()));
-n.load(JSON.parse(readFileSync('./model.json').toString()));
-let losses: numberArray = [];
-for (let i = 0; i < trainingData.length; i++) {
-  let pred = n.feedforward(trainingData[i].input)[0];
-  let real = trainingData[i].output[0];
-  losses.push(Math.pow(pred - real, 2));
-}
-let mse = sum(losses) / losses.length;
-console.log("mse", mse);
+// writeFileSync('./model.json', JSON.stringify(n.snapshot()));
+// n.load(JSON.parse(readFileSync('./model.json').toString()));
+// let losses: numberArray = [];
+// for (let i = 0; i < trainingData.length; i++) {
+//   let pred = n.feedforward(trainingData[i].input)[0];
+//   let real = trainingData[i].output[0];
+//   losses.push(Math.pow(pred - real, 2));
+// }
+// let mse = sum(losses) / losses.length;
+// console.log("mse", mse);
 for(let i of trainingData){
   console.log(n.feedforward(i.input));
 }
